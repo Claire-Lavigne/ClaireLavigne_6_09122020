d311c37e16639c1358d992b5362ff7ec0956bf23
Claire Lavigne <45535173+Claire-Lavigne@users.noreply.github.com>
2022-06-30 10:45:02 +0200

    Update media.class.js

--- js/media.class.js
+++ js/media.class.js
@@ -37,16 +37,16 @@ export default class Media {
     let mediaLightbox = `
       <div id="${this.media.id}" class="lightbox-content">
         <button class="lightbox-close" title="Close dialog">
-          <img class="icon-close" src="./assets/cross-alt.png" alt="icon close">
+          <img class="icon-close" src="https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/cross-alt.png" alt="icon close">
         </button>
         <div class="lightbox-body">${this.media.generateView()}
           <div class="media-infos">${this.media.generateTitle()}</div>
         </div>
         <a class="lightbox__prev" title="Previous image">
-          <img class="icon-prev" src="./assets/prev.png" alt="previous arrow">
+          <img class="icon-prev" src="https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/prev.png" alt="previous arrow">
         </a>
         <a class="lightbox__next" title="Next image">
-          <img class="icon-next" src="./assets/next.png" alt="next arrow">
+          <img class="icon-next" src="https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/next.png" alt="next arrow">
         </a>
       </div>
       `;
